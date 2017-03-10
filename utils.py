9fe6c5874544beb643d80e063ff963e1a05b64b7
Ondrej Grover <ondrej.grover@gmail.com>
2017-03-10 11:14:34 +0100

    return average sampling and check if sampling is even

--- utils.py
+++ utils.py
@@ -1,3 +1,11 @@
+import warnings
+
+
+class UnevenSamplingWarning(Warning):
+    pass
+# always (not just once) show decimation warnings to see the responsible signal
+warnings.filterwarnings('always', category=UnevenSamplingWarning)
+
 
 def get_maybe_last_dim_axis(darray, dim=None):
     if dim is None:
@@ -8,7 +16,13 @@ def get_maybe_last_dim_axis(darray, dim=None):
     return dim, axis
 
 
-def get_sampling_step(darray, dim=None):
+def get_sampling_step(darray, dim=None, rtol=1e-5):
     dim, axis = get_maybe_last_dim_axis(darray, dim)
     coord = darray.coords[dim]
-    return float(coord[1] - coord[0])
+    dt_avg = float(coord[-1] - coord[0]) / len(coord)
+    dt_first = float(coord[1] - coord[0])
+    if abs(dt_avg - dt_first) > rtol * min(dt_first, dt_avg):
+        # show warning at caller level to see which signal it is related to
+        warnings.warn('Average sampling {} != first sampling step {}'.format(
+            dt_avg, dt_first), UnevenSamplingWarning, stacklevel=2)
+    return dt_avg               # should be more precise
