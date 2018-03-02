Bugfix in np.gradient

xrscipy/numpy_.py
@@ -6,14 +6,14 @@ from . import errors
 def gradient(f, dim, edge_order=1):
     errors.raise_not_sorted(f[dim])
 
+    x = f[dim]
+    dim = x.dims[0]
     output_core_dim = [dim]
 
     if isinstance(f, xr.DataArray):
-        result = xr.apply_ufunc(
-                    np.gradient, f, input_core_dims=[[dim]],
-                    output_core_dims=[output_core_dim],
-                    kwargs={'edge_order': edge_order, 'axis': -1})
-        return result.transpose(*f.dims)
-
+        # TODO use apply_ufunc
+        result = np.gradient(f.values, x.values, axis=f.get_axis_num(dim),
+                             edge_order=edge_order)
+        return xr.DataArray(result, dims=f.dims, coords=f.coords)
     else:
         raise TypeError('Invalid data type {} is given.'.format(type(f)))

xrscipy/tests/test_numpy.py
@@ -10,29 +10,34 @@ from xrscipy import gradient
 def get_da(ndim, ascend=False):
     shapes = [10, 11, 12]
     dims = ['x', 'y', 'z']
-    coords = {'x': np.arange(shapes[0]), 'z': np.linspace(0, 1, shapes[2])}
+    coords = {}
+    coords['x'] = np.arange(shapes[0]) * 0.2
+    if ndim >= 2:
+        coords['z'] = np.linspace(0, 1, shapes[2])
+    coords['time'] = ('x', ), np.linspace(0, 1, shapes[0])
     da = xr.DataArray(np.random.randn(*shapes[:ndim]), dims=dims[:ndim],
-                      coords={k: v for k, v in coords.items() if k in
-                              dims[:ndim]})
+                      coords=coords)
     da.attrs['comment'] = 'dummy comment.'
     return da
 
 
 @pytest.mark.parametrize('ndim', [1, 3])
 @pytest.mark.parametrize('edge_order', [1, 2])
-def test_gradient(ndim, edge_order):
+@pytest.mark.parametrize('dim', ['x', 'time'])
+def test_gradient(ndim, edge_order, dim):
     da = get_da(ndim)
-    dim = 'x'
-    axis = da.get_axis_num(dim)
+
+    axis = da.get_axis_num(da[dim].dims[0])
     actual = gradient(da, dim, edge_order=edge_order)
     expected = np.gradient(da.values, da[dim].values, edge_order=edge_order,
                            axis=axis)
+
     assert (actual.values == expected).all()
 
     # make sure the original data does not change
     da.values.ndim == ndim
 
     # make sure the coordinate is propagated
-    for key in da.coords:
-        if key != 'x':
+    for key, v in da.coords.items():
+        if 'x' not in v.dims:
             assert da[key].identical(actual[key])