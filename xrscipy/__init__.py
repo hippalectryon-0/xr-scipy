fe5db775599d8a86940cf75bbaeff9e2942ea4bf
Keisuke Fujii <fujiisoup@gmail.com>
2018-03-03 09:40:12 +0900

    Expose fftpack.

--- xrscipy/__init__.py
+++ xrscipy/__init__.py
@@ -3,5 +3,5 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from . import integrate, interpolate
+from . import integrate, interpolate, fftpack
 from .numpy_ import gradient
