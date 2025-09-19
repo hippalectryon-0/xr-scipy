8c8a47665448b28e8f8aab9e7ad9494484a43a2f
Hippalectryon <hippalectryon@protonmail.com>
2025-09-19 17:43:35 +0900

    black > ruff

--- xrscipy/signal/__init__.py
+++ xrscipy/signal/__init__.py
@@ -1,5 +1,7 @@
 """init"""
 
+__all__ = ["decimate", "savgol_filter", "coherence", "csd", "hilbert", "welch", "spectrogram"]
+
 from xrscipy.signal.filters import decimate, savgol_filter
 from xrscipy.signal.spectral import coherence, csd, hilbert
 from xrscipy.signal.spectral import psd as welch
--- xrscipy/signal/extra/__init__.py
+++ xrscipy/signal/extra/__init__.py
@@ -1,7 +1,18 @@
 """functions which don't wrap a corresponding scipy function"""
 
-# noinspection PyUnresolvedReferences
-from xrscipy.signal.filters import bandpass, bandstop, frequency_filter, highpass, lowpass
+__all__ = [
+    "bandpass",
+    "bandstop",
+    "frequency_filter",
+    "highpass",
+    "lowpass",
+    "coherogram",
+    "crossspectrogram",
+    "freq2lag",
+    "psd",
+    "xcorrelation",
+]
 
-# noinspection PyUnresolvedReferences
+
+from xrscipy.signal.filters import bandpass, bandstop, frequency_filter, highpass, lowpass
 from xrscipy.signal.spectral import coherogram, crossspectrogram, freq2lag, psd, xcorrelation
